17707c5791cc019cbfc8fc345ddf705edf6d623a
Kodai Yamasaki <114902604+ymski@users.noreply.github.com>
2023-01-26 17:46:03 +0900

    tmp

--- intra_process_demo/src/cyclic_pipeline/two_pub_cyclic_pipeline.cpp
+++ intra_process_demo/src/cyclic_pipeline/two_pub_cyclic_pipeline.cpp
@@ -31,21 +31,19 @@ struct IncrementerPipe : public rclcpp::Node
   : Node(name, rclcpp::NodeOptions().use_intra_process_comms(true))
   {
     // Create a publisher on the output topic.
-    pub = this->create_publisher<std_msgs::msg::Int32>(out, 1);
-    pub__ = this->create_publisher<std_msgs::msg::Int32>(out, 1);
+    pub = this->create_publisher<std_msgs::msg::Int32>(out, 10);
     std::weak_ptr<std::remove_pointer<decltype(pub.get())>::type> captured_pub = pub;
-    std::weak_ptr<std::remove_pointer<decltype(pub.get())>::type> captured_pub__ = pub__;
     // Create a subscription on the input topic.
     sub = this->create_subscription<std_msgs::msg::Int32>(
       in,
-      1,
+      10,
       [captured_pub](std_msgs::msg::Int32::UniquePtr msg) {
-        auto pub_ptr = captured_pub.lock();	
+        auto pub_ptr = captured_pub.lock();
         if (!pub_ptr) {
           return;
         }
         printf(
-          "SUB1: Received message with value:         %d, and address: 0x%" PRIXPTR "\n", msg->data,
+          "Received message with value:         %d, and address: 0x%" PRIXPTR "\n", msg->data,
           reinterpret_cast<std::uintptr_t>(msg.get()));
         printf("  sleeping for 1 second...\n");
         if (!rclcpp::sleep_for(1s)) {
@@ -54,34 +52,21 @@ struct IncrementerPipe : public rclcpp::Node
         printf("  done.\n");
         msg->data++;    // Increment the message's data.
         printf(
-          "SUB1: Incrementing and sending with value: %d, and address: 0x%" PRIXPTR "\n", msg->data,
+          "Incrementing and sending with value: %d, and address: 0x%" PRIXPTR "\n", msg->data,
           reinterpret_cast<std::uintptr_t>(msg.get()));
         pub_ptr->publish(std::move(msg));    // Send the message along to the output topic.
-  	std::unique_ptr<std_msgs::msg::Int32> msg__(new std_msgs::msg::Int32());
-  	msg__->data = 100;
-       	pub__->publish(std::move(msg__));	
       });
-    
   }
 
   rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr pub;
-  rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr pub__;
   rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr sub;
 };
-
-static void single_thread_spin(std::shared_ptr<rclcpp::Node> node) {
-    auto exec = rclcpp::executors::SingleThreadedExecutor();
-    exec.add_node(node);
-    exec.spin();
-}
-
 
 int main(int argc, char * argv[])
 {
   setvbuf(stdout, NULL, _IONBF, BUFSIZ);
   rclcpp::init(argc, argv);
-  rclcpp::executors::SingleThreadedExecutor executor1;
-  rclcpp::executors::SingleThreadedExecutor executor2;
+  rclcpp::executors::SingleThreadedExecutor executor;
 
   // Create a simple loop by connecting the in and out topics of two IncrementerPipe's.
   // The expectation is that the address of the message being passed between them never changes.
@@ -96,14 +81,9 @@ int main(int argc, char * argv[])
     reinterpret_cast<std::uintptr_t>(msg.get()));
   pipe1->pub->publish(std::move(msg));
 
-  // executor1.add_node(pipe1);
-  // executor2.add_node(pipe2);
-  // std::thread th1 (executor1.spin);
-  // std::thread th2 (executor2.spin);
-  std::thread th1 (single_thread_spin, pipe1);
-  std::thread th2 (single_thread_spin, pipe2);
-  th1.join();
-  th2.join();
+  executor.add_node(pipe1);
+  executor.add_node(pipe2);
+  executor.spin();
 
   rclcpp::shutdown();
 
